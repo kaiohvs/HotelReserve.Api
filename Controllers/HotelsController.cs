47c1f4a88d44c8264fc9d6d4a2bcb4e1f0d426b2
Kaio Vasconcelos <Kaiohvs@gmail.com>
2024-07-10 16:00:47 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HotelsController.cs
@@ -0,0 +1,74 @@
+﻿using HotelReserve.Api.Data;
+using HotelReserve.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HotelsController : ControllerBase
+{
+    private readonly HotelContext _context;
+
+    public HotelsController(HotelContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+    {
+        return await _context.Hotels.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Hotel>> GetHotel(int id)
+    {
+        var hotel = await _context.Hotels.FindAsync(id);
+
+        if (hotel == null)
+        {
+            return NotFound();
+        }
+
+        return hotel;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+    {
+        _context.Hotels.Add(hotel);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetHotel), new { id = hotel.Id }, hotel);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+    {
+        if (id != hotel.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(hotel).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteHotel(int id)
+    {
+        var hotel = await _context.Hotels.FindAsync(id);
+
+        if (hotel == null)
+        {
+            return NotFound();
+        }
+
+        _context.Hotels.Remove(hotel);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ Data/HotelContext.cs
@@ -0,0 +1,12 @@
+﻿using HotelReserve.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReserve.Api.Data
+{
+    public class HotelContext : DbContext
+    {
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options) { }
+
+        public DbSet<Hotel> Hotels{ get; set; }
+    }
+}
